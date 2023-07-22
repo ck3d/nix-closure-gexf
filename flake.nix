d6fda60035d67b07ea674de64ed068dab186b2a4
Christian Kögler <ck3d@gmx.de>
2023-07-22 13:53:51 +0200

    flake/inputs: Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,17 @@
 {
   description = "nix-closure-gexf flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.05";
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      { packages.default = pkgs.callPackage ./. { }; }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          default = pkgs.callPackage ./. { };
+        });
+    };
 }
